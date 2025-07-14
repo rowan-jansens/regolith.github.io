4c9f2aa1a09bef11eb05ca5ad584e86cece0f2f1
Rowan Jansens <67484064+rowan-jansens@users.noreply.github.com>
2025-07-14 19:13:36 -0400

    Update animation.js

--- animation.js
+++ animation.js
@@ -1,46 +1,105 @@
-const accelamount = 0.2;
+var duration1 = $('#video1').height();
+var video1 = $('#video1').get(0);
+var progressvalue1 = 0; //tracks the position of the scroll
+video1.src = "images/herospin2.mp4";
+video1.muted = true;
+video1.setAttribute("playsinline", "");
+video1.setAttribute("autoplay", "");
+video1.load();
 
-function setupScrollControlledVideo(videoId, viewId, videoSrc) {
-  const video = document.getElementById(videoId);
-  const view = document.getElementById(viewId);
-  let progress = 0;
-  let delay = 0;
+video1.onloadedmetadata = function () {
+  console.log('metadata loaded!');
+  video1.play().catch(() => {});
+  const view2 = document.getElementById("view2");
+  const main = document.getElementById("main");
+  var controller = new ScrollMagic.Controller();
 
-  video.src = videoSrc;
-  video.muted = true;
-  video.setAttribute('playsinline', '');
-  video.setAttribute('autoplay', '');
+  var scene1 = new ScrollMagic.Scene({
+    duration: 6 * duration1,
+    triggerElement: view2,
+    triggerHook: 0.5,
+    offset: video1.offsetHeight / 2
+  })
+    .setPin(view2)
+    .addTo(controller)
+    .on("progress", function (e) {
+      progressvalue1 = Math.floor(100 * e.progress);
+    });
+};
 
-  video.load(); // ensures proper loading on iOS
+let accelamount = 0.2;
+let delay1 = 0;
+setInterval(() => {
+  delay1 += (progressvalue1 - delay1) * accelamount;
+  video1.currentTime = video1.duration * delay1 / 100 - 0.01;
+}, 20);
 
-  video.onloadedmetadata = () => {
-    video.play().catch(() => {}); // Required by iOS to activate autoplay via user interaction
+var duration2 = $('#video2').height();
+var video2 = $('#video2').get(0);
+var progressvalue2 = 0;
+video2.src = "images/z_spin.mp4";
+video2.muted = true;
+video2.setAttribute("playsinline", "");
+video2.setAttribute("autoplay", "");
+video2.load();
 
-    const videoDuration = video.duration;
-    const height = view.offsetHeight;
+video2.onloadedmetadata = function () {
+  console.log('metadata loaded!');
+  video2.play().catch(() => {});
+  const view3 = document.getElementById("view3");
+  const main = document.getElementById("main");
+  var controller2 = new ScrollMagic.Controller();
 
-    const controller = new ScrollMagic.Controller();
-    new ScrollMagic.Scene({
-      duration: 4 * height,
-      triggerElement: view,
-      triggerHook: 0.5,
-      offset: video.offsetHeight / 2
-    })
-      .setPin(view)
-      .addTo(controller)
-      // .addIndicators()
-      .on("progress", e => {
-        progress = Math.floor(100 * e.progress);
-      });
+  var scene2 = new ScrollMagic.Scene({
+    duration: 6 * duration2,
+    triggerElement: view3,
+    triggerHook: 0.5,
+    offset: video2.offsetHeight / 2
+  })
+    .setPin(view3)
+    .addTo(controller2)
+    .on("progress", function (e) {
+      progressvalue2 = Math.floor(100 * e.progress);
+    });
+};
 
-    setInterval(() => {
-      delay += (progress - delay) * accelamount;
-      video.currentTime = videoDuration * delay / 100 - 0.01;
-    }, 20);
-  };
-}
+let delay2 = 0;
+setInterval(() => {
+  delay2 += (progressvalue2 - delay2) * accelamount;
+  video2.currentTime = video2.duration * delay2 / 100 - 0.01;
+}, 20);
 
-// Setup all videos
-setupScrollControlledVideo("video1", "view2", "images/herospin2.mp4");
-setupScrollControlledVideo("video2", "view3", "images/z_spin.mp4");
-setupScrollControlledVideo("video3", "view4", "images/paralax2.mp4");
+var duration3 = $('#video3').height();
+var video3 = $('#video3').get(0);
+var progressvalue3 = 0;
+video3.src = "images/paralax2.mp4";
+video3.muted = true;
+video3.setAttribute("playsinline", "");
+video3.setAttribute("autoplay", "");
+video3.load();
+
+video3.onloadedmetadata = function () {
+  console.log('metadata loaded!');
+  video3.play().catch(() => {});
+  const view4 = document.getElementById("view4");
+  const main = document.getElementById("main");
+  var controller3 = new ScrollMagic.Controller();
+
+  var scene3 = new ScrollMagic.Scene({
+    duration: 6 * duration3,
+    triggerElement: view4,
+    triggerHook: 0.5,
+    offset: video3.offsetHeight / 2
+  })
+    .setPin(view4)
+    .addTo(controller3)
+    .on("progress", function (e) {
+      progressvalue3 = Math.floor(100 * e.progress);
+    });
+};
+
+let delay3 = 0;
+setInterval(() => {
+  delay3 += (progressvalue3 - delay3) * accelamount;
+  video3.currentTime = video3.duration * delay3 / 100 - 0.01;
+}, 20);
